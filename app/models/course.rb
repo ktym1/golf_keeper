3345717457e984ac1232c8b35d2a68d82e8cf9cf
Kerry Mui <kerry.t.mui@gmail.com>
2014-09-30 14:09:17 -0400

    added associations for course and hole models, added validations for course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,6 @@
 class Course < ActiveRecord::Base
+	has_many :holes
+
+	validates :name, presence: true
+	validates :hole_id, presence: true, numericality: {only_integer: true}
 end
--- app/models/hole.rb
+++ app/models/hole.rb
@@ -1,2 +1,6 @@
 class Hole < ActiveRecord::Base
+	belongs_to :course
+
+	# validates :round_9 nil?
+	# validates :round_18 nil?
 end
