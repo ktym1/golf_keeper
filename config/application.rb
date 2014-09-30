0332ae8d1ed15ff37ddc13c021163f0d849d72a7
Kerry Mui <kerry.t.mui@gmail.com>
2014-09-30 12:16:54 -0400

    deleted test directory, cnofigured rspec fixtures and specs in application file

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,15 @@ module GolfKeeper
     # For Foundation 5
     config.assets.precompile += %w( vendor/modernizr )
 
+    config.generators do |g|
+        g.test_framework :rspec,
+            fixtures: true,
+            view_specs: false,
+            helper_specs: false,
+            routing_specs: false,
+            controller_specs: true,
+            request_specs: false
+        g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
