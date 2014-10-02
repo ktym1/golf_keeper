c1566774e9f47198f6cf3dda014119a9edd7b113
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-02 16:25:45 -0400

    commented out hole_id validation ufn

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,5 +2,5 @@ class Course < ActiveRecord::Base
 	has_many :holes
 
 	validates :name, presence: true
-	validates :hole_id, presence: true, numericality: {only_integer: true}
+	# validates :hole_id, presence: true, numericality: {only_integer: true}
 end
