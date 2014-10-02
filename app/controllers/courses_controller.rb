4b4f7b5e599eb468f9ab8c3c722df13359940683
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-01 22:03:33 -0400

    added index action for courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,19 @@
 class CoursesController < ApplicationController
+
+	def index
+		@courses = Course.all
+	end
+
+	# def new
+	# 	@course = Course.new
+	# end
+
+	# def create
+	# 	@course = Course.new(course_params)
+	# 	if @course.save
+	# 		redirect_to courses_url
+	# 	else
+	# 		render :new
+	# 	end
+	# end
 end
