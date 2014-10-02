adc69282b255214b9939b4e3655186492a3f22a8
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-01 22:09:36 -0400

    updated routes with root to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :courses do
   end
+  root :to => "courses#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
