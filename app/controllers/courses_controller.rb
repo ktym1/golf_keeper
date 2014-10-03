e6da2573a08a780d0a8eb85ef8a24fb43a5285bc
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-03 09:22:28 -0400

    added create action in controller, and corresponding new form view w/ updated index view

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -8,12 +8,17 @@ class CoursesController < ApplicationController
 		@course = Course.new
 	end
 
-	# def create
-	# 	@course = Course.new(course_params)
-	# 	if @course.save
-	# 		redirect_to courses_url
-	# 	else
-	# 		render :new
-	# 	end
-	# end
+	def create
+		@course = Course.new(course_params)
+		if @course.save
+			redirect_to courses_url
+		else
+			render :new
+		end
+	end
+
+	private
+	def course_params
+		params.require(:course).permit(:name)
+	end
 end
