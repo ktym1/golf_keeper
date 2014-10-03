d82a00f5048309d96eca0dfd0ad65a37113394d9
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-03 09:31:01 -0400

    added show action controller and corresponding view

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -17,6 +17,10 @@ class CoursesController < ApplicationController
 		end
 	end
 
+	def show 
+		@course = Course.find(params[:id])
+	end
+
 	private
 	def course_params
 		params.require(:course).permit(:name)
