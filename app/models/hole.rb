b9e7a11b38c85db1cb1c37c447bd6bbf06cc5174
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-03 21:49:56 -0400

    added course_id attribute validation for hole model

--- app/models/hole.rb
+++ app/models/hole.rb
@@ -1,6 +1,7 @@
 class Hole < ActiveRecord::Base
 	belongs_to :course
 
+	validates :course_id, presence: true, numericality: {only_integer: true}
 	# validates :round_9 nil?
 	# validates :round_18 nil?
 end
