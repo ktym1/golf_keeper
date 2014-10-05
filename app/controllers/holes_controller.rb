b870f390ed95e3e58f4071aff190e032ea2057e9
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-05 12:35:41 -0400

    added create action in holes controller to allow selection of 9 or 18 holes attribute form in show view

--- app/controllers/holes_controller.rb
+++ app/controllers/holes_controller.rb
@@ -3,4 +3,19 @@ class HolesController < ApplicationController
 		@hole = Hole.find(params[:id])
 	end
 
+	def create
+		@course = Course.find(params[:course_id])
+
+		@hole = @course.holes.create(holes_params)
+		if @holes.save
+			redirect_to course_path(@course), notice: "Hole created successfully"
+		else
+			render "courses/show"
+		end
+	end
+
+	private
+		def hole_params
+			params.require(:hole).permit(:course_id, :round_9, :round_18)
+		end
 end
