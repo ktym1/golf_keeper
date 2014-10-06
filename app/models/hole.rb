bca9397dd12bcf5ba3e63926c4d8cd8ed3b2db61
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-06 18:51:07 -0400

    added par integer validation to hole model

--- app/models/hole.rb
+++ app/models/hole.rb
@@ -2,6 +2,5 @@ class Hole < ActiveRecord::Base
 	belongs_to :course
 
 	validates :course_id, presence: true, numericality: {only_integer: true}
-	# validates :round_9 nil?
-	# validates :round_18 nil?
+	validates :par, presence: true, numericality: {only_integer: true}
 end
