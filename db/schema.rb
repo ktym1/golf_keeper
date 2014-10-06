9f9035b438d25fb20495dc6b1e74fd9ecf68dd40
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-06 16:47:00 -0400

    added par attribute to hole model w/ migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141006192826) do
+ActiveRecord::Schema.define(version: 20141006202949) do
 
   create_table "courses", force: true do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20141006192826) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "course_id"
+    t.integer  "par"
   end
 
 end
