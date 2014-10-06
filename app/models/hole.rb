4726f8c9b812d1847e56c15595b3ce5a3b6c4a8f
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-06 19:25:22 -0400

    incomplete pseudo code for hole count restriction methods

--- app/models/hole.rb
+++ app/models/hole.rb
@@ -3,4 +3,17 @@ class Hole < ActiveRecord::Base
 
 	validates :course_id, presence: true, numericality: {only_integer: true}
 	validates :par, presence: true, numericality: {only_integer: true}
+
+	def new_hole_count
+		if @course is new
+			@hole begins at 1
+		end
+	end
+	
+	def hole_max
+		if @hole.self > 18
+			render: "courses/how"
+		else
+			save
+	end
 end
