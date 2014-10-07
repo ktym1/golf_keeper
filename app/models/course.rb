63d18ca7f666e7237507bbcd05796cd3c6997970
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-07 11:19:41 -0400

    added relationships in course and round models, added validation in course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ActiveRecord::Base
 	has_many :holes
-
+	has_many :rounds
 	validates :name, presence: true
 	# validates :hole_id, presence: true, numericality: {only_integer: true}
 end
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,2 +1,6 @@
 class Round < ActiveRecord::Base
+	belongs_to :course
+
+	validates :course_id, presence: true, numericality: {only_integer: true}
+	validates :round_length, presence: true, numericality: {only_integer: true}
 end
