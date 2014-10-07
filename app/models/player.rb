617cdce562e33c864c462e013d5a4eeaa5e4bbcf
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-07 11:48:08 -0400

    added relationships in round and player models. added corresponding validations

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,6 @@
 class Player < ActiveRecord::Base
+	has_many :rounds
+
+	validates :username, presence: true
+	validates :email, presence: true
 end
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,6 +1,8 @@
 class Round < ActiveRecord::Base
 	belongs_to :course
+	belongs_to :player
 
 	validates :course_id, presence: true, numericality: {only_integer: true}
 	validates :round_length, presence: true, numericality: {only_integer: true}
+	validates :course_id, presence: true, numericality: {only_integer: true
 end
