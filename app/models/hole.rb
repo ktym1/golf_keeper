78dce9acd90c182b1995731c1a1e65a11945dcf6
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-07 09:33:53 -0400

    commented out incomplete validation methods in hole model

--- app/models/hole.rb
+++ app/models/hole.rb
@@ -4,16 +4,16 @@ class Hole < ActiveRecord::Base
 	validates :course_id, presence: true, numericality: {only_integer: true}
 	validates :par, presence: true, numericality: {only_integer: true}
 
-	def new_hole_count
-		if @course is new
-			@hole begins at 1
-		end
-	end
+	# def new_hole_count
+	# 	if @course is new
+	# 		@hole begins at 1
+	# 	end
+	# end
 	
-	def hole_max
-		if @hole.self > 18
-			render: "courses/how"
-		else
-			save
-	end
+	# def hole_max
+	# 	if @hole.self > 18
+	# 		render: "courses/how"
+	# 	else
+	# 		save
+	# end
 end
