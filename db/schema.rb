a030fb7f846d66a776cafe53bba552bc4072d632
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-07 15:25:55 -0400

    generated score model and corresponding specs/factories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007154522) do
+ActiveRecord::Schema.define(version: 20141007192330) do
 
   create_table "courses", force: true do |t|
     t.string   "name"
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 20141007154522) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "player_id"
+  end
+
+  create_table "scores", force: true do |t|
+    t.integer  "hole_id"
+    t.integer  "score"
+    t.integer  "round_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
---
+++ spec/factories/scores.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :score do
+    hole_id 1
+    score 1
+    round_id 1
+  end
+end
