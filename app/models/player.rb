cbde5dedc47124e1ff452390d0e8ada96c6be760
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-08 16:56:06 -0400

    added a password_digest column to players model

--- app/models/player.rb
+++ app/models/player.rb
@@ -5,4 +5,5 @@ class Player < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 30 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
+	has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007192330) do
+ActiveRecord::Schema.define(version: 20141008182025) do
 
   create_table "courses", force: true do |t|
     t.string   "name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20141007192330) do
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
   create_table "rounds", force: true do |t|
