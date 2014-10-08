5028170371318bb33f20bd4a842d85ffb187d7cb
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-08 12:18:23 -0400

    added player email format validation w/ regex constant

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,5 +2,7 @@ class Player < ActiveRecord::Base
 	has_many :rounds
 
 	validates :username, presence: true, length: { maximum: 20 }
-	validates :email, presence: true, length: { maximum: 30 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 30 }, format: { with: VALID_EMAIL_REGEX }
+
 end
