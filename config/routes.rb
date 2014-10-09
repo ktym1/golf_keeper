2ef7524b2bc432cdd241e42b3a07d64c86d773e0
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-09 16:01:37 -0400

    added custom route signup for new player form

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
     resources :holes
   end
   root :to => "courses#index"
+
+  get 'signup'  => 'players#new'
   resources :players
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
