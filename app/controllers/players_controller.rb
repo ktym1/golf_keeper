07900b4698047420e67cb64628c7504c1913f93d
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-09 16:05:48 -0400

    added create action and strong params in players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,5 +3,17 @@ class PlayersController < ApplicationController
   	@player = Player.new
   end
 
+  def create
+  	@player = Player.new(player_params)
+  	if @player.save
+  		redirect_to players_url, :notice => "Signed up!"
+  	else
+  		render 'new'
+  	end
+  end  
 
+  private
+  def player_params
+  	params.require(:player).permit(:email, :username, :password, :password_confirmation)
+  end
 end
