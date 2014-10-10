691781862de3bef35cbfc0277259e0cec1ec632a
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-10 16:01:21 -0400

    generated a sessions controller, added sessions routes including custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
   end
   root :to => "courses#index"
 
-  get 'signup'  => 'players#new'
+
   resources :players
+  resources :sessions, only: [:new, :create, :destroy]
+  match 'signup',  to: 'players#new', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
