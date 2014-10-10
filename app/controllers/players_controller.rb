8f0ff2c714c1e363ea5d0f9428fc5dbf3f5507a7
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-10 12:23:20 -0400

    added show action in controller, updated new player form with updated partial for errors, created errors partial, updated show view

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,12 +6,17 @@ class PlayersController < ApplicationController
   def create
   	@player = Player.new(player_params)
   	if @player.save
-  		redirect_to players_url, :notice => "Signed up!"
+  		redirect_to '/players/:id', :notice => "Signed up!"
   	else
   		render 'new'
   	end
   end  
 
+  def show
+    @player = Player.find(params[:id])
+
+  end
+  
   private
   def player_params
   	params.require(:player).permit(:email, :username, :password, :password_confirmation)
