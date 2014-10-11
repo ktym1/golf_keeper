5ce9c0e4f48caf8a010d90851f688b914e177de6
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-10 22:05:15 -0400

    updated create action in sessions controller with authentication client boolean, added SessionsHelper in application controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		player = Player.find_by(email: params[:session][:email].downcase)
+		if player && player.authenticate(params[:session][:password])
+			sign_in player
+			redirect_to player
+		else
+			flash.now[:error] = 'Invalid email/password combination'
+			render 'new'
+		end
 	end
 
 	def destroy
