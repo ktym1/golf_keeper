358159c56354abb4db82a88af354daaa90eadb94
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-10 23:49:28 -0400

    added sign in method in SessionsHelper module

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+
+	def sign_in(player)
+		remember_token = Player.new_remember_token
+		cookies.permanent[:remember_token] = remember_token
+		player.update_attribute(:remember_token, Player.digest(remember_token))
+		self.current_user = player
+	end
 end
