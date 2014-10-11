b2f56f91e6cce33c69f136d2379c90f3d9f6899b
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-10 22:07:17 -0400

    generated migration for a remember token column and index in players model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141008182025) do
+ActiveRecord::Schema.define(version: 20141011020543) do
 
   create_table "courses", force: true do |t|
     t.string   "name"
@@ -32,7 +32,10 @@ ActiveRecord::Schema.define(version: 20141008182025) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
+
+  add_index "players", ["remember_token"], name: "index_players_on_remember_token"
 
   create_table "rounds", force: true do |t|
     t.integer  "course_id"
