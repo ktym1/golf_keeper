c86ad2a38a31ede54c5fa7c3bc9bb2fd72d80422
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-13 18:00:07 -0400

    updated player controller to redirect a newly signed up use to their profile

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,7 +6,9 @@ class PlayersController < ApplicationController
   def create
   	@player = Player.new(player_params)
   	if @player.save
-  		redirect_to signin_path, :notice => "Signed up!"
+      sign_in @player
+      flash[:success] = "Welcome to the Sample App"
+  		redirect_to @player
   	else
   		render 'new'
   	end
