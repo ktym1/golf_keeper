40d6c32c276282736f5f1aabda0911c6b453a8f5
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-13 17:53:00 -0400

    updated player controller create action to redirect to signin page once player saved

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,7 +6,7 @@ class PlayersController < ApplicationController
   def create
   	@player = Player.new(player_params)
   	if @player.save
-  		redirect_to '/players/:id', :notice => "Signed up!"
+  		redirect_to signin_path, :notice => "Signed up!"
   	else
   		render 'new'
   	end
