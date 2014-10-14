2d0bb783c589d50881290c0193a591b956218a46
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-14 15:43:44 -0400

    commented out current user helper in sign_in method to allow signing in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,7 +4,7 @@ module SessionsHelper
 		remember_token = Player.new_remember_token
 		cookies.permanent[:remember_token] = remember_token
 		player.update_attribute(:remember_token, Player.digest(remember_token))
-		self.current_user = player
+		# self.current_user = player
 	end
 
 	def sign_out
@@ -24,4 +24,13 @@ module SessionsHelper
 		remember_token = Player.digest(cookies[:remember_token])
 		@current_user ||= Player.find_by(remember_token: remember_token)
 	end
+
+	# def redirect_back_or(default)
+	# 	redirect_to(session[:return_to] || default)
+	# 	session.delete(:return_to)
+	# end
+
+	# def store_location
+	# 	session[:return_to] = request.url if request.get?
+	# end
 end
