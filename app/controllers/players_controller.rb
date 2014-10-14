a6562da757e802a0c87ba235e049fb18b0bd053d
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-14 10:35:20 -0400

    added update action to player controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -22,6 +22,16 @@ class PlayersController < ApplicationController
     @player = Player.find(params[:id])
   end
   
+  def update
+    @player = Player.find(params[:id])
+    if @player.update_attributes(player_params)
+      flash[:success] = "Profile updated"
+      redirect_to @player
+    else
+      render "edit"
+    end
+  end
+ 
   private
   def player_params
   	params.require(:player).permit(:email, :username, :password, :password_confirmation)
