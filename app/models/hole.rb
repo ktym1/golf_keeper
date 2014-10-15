5a4326ec91d4b544dde69013912688ac1d527e83
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-15 18:54:45 -0400

    added hole_number validation for 1..18

--- app/models/hole.rb
+++ app/models/hole.rb
@@ -4,6 +4,15 @@ class Hole < ActiveRecord::Base
 
 	validates :course_id, presence: true, numericality: {only_integer: true}
 	validates :par, presence: true, numericality: {only_integer: true}
+	validates :hole_number, inclusion: { in: 1..18, message: "Must be from 1 to 18" } 
+	# validate :course_should_exist
+
+	# def course_should_exist
+	# 	if course.blank?
+	# 		errors.base.add("Course does not exist.")
+	# 		return false
+	# 	end
+	# end
 
 	# def new_hole_count
 	# 	if @course is new
