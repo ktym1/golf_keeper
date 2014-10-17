3e37e7d505ca6bfc160e89d9618f8d4e9bddbf1d
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-17 11:58:26 -0400

    deleted tee_marker_hole model bc of naming convention.

--- app/models/tee_marker_hole.rb
+++
@@ -1,9 +0,0 @@
-class TeeMarkerHole < ActiveRecord::Base
-belongs_to :hole
-
-validates :championship, presence: true, numericality: {only_integer: true}
-validates :blue, presence: true, numericality: {only_integer: true}
-validates :white, presence: true, numericality: {only_integer: true}
-validates :red, presence: true, numericality: {only_integer: true}
-validates :hole_id, presence: true, numericality: {only_integer: true}	
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141017150918) do
+ActiveRecord::Schema.define(version: 20141017155255) do
 
   create_table "courses", force: true do |t|
     t.string   "name"
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 20141017150918) do
     t.datetime "updated_at"
   end
 
-  create_table "tee_marker_holes", force: true do |t|
+  create_table "teemarkerholes", force: true do |t|
     t.integer  "championship"
     t.integer  "blue"
     t.integer  "white"
