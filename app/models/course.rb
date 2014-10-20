55584ec3ca22d07141b8c52777ee19ef9d720939
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-20 15:53:03 -0400

    removed legacy validation

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,8 +2,7 @@ class Course < ActiveRecord::Base
 	has_many :holes
 	has_many :rounds
 	has_many :tees
-	
+
 	validates :name, presence: true
-	validates :handicap_course, presence: true, numericality: {only_integer: true}
-	# validates :hole_id, presence: true, numericality: {only_integer: true}
+	
 end
