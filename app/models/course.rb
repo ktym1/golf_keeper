1948b23b4470f3c37c3e9c4a679eaec9ea2e6cea
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-20 10:07:30 -0400

    added relationships between model and tees model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,8 @@
 class Course < ActiveRecord::Base
 	has_many :holes
 	has_many :rounds
-
+	has_many :tees
+	
 	validates :name, presence: true
 	validates :handicap_course, presence: true, numericality: {only_integer: true}
 	# validates :hole_id, presence: true, numericality: {only_integer: true}
