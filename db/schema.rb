83595efbfb95e0b1bab37256eee549240d6b5e3a
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-20 15:08:12 -0400

    removed course_id column from ratings table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141020190522) do
+ActiveRecord::Schema.define(version: 20141020190721) do
 
   create_table "courses", force: true do |t|
     t.string   "name"
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 20141020190522) do
   create_table "ratings", force: true do |t|
     t.integer  "course_rating"
     t.integer  "slope_rating"
-    t.integer  "course_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
