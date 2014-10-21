a20c3c18a50049e64b3a8b249df930145477191e
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-20 22:35:26 -0400

    added gender validation to player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -8,6 +8,8 @@ class Player < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 30 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
+	validates :gender, :inclusion => ["m", "f", "M", "F"]
+
 	has_secure_password
 	validates :password, length: { minimum: 6}
 
