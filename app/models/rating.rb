28530eaa5a4446319cb322ee7a732e6b2c51379b
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-20 22:35:56 -0400

    added validations to rating model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,9 @@
 class Rating < ActiveRecord::Base
-	belongs_to :tee
+belongs_to :tee
+
+validates :course_rating, presence: true, numericality: {only_integer: true}
+validates :slope_rating, presence: true, numericality: {only_integer: true}
+validates :tee_id, presence: true, numericality: {only_integer: true}
+validates :gender, :inclusion => ["m", "f", "M", "F"]
+
 end
