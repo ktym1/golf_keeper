72ee3d69cd347ce27a1329ab0ff58e1113398a45
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-20 22:59:50 -0400

    updated course_rating validation in rating model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,7 +1,7 @@
 class Rating < ActiveRecord::Base
 belongs_to :tee
 
-validates :course_rating, presence: true, numericality: {only_integer: true}
+validates :course_rating, presence: true
 validates :slope_rating, presence: true, numericality: {only_integer: true}
 validates :tee_id, presence: true, numericality: {only_integer: true}
 validates :gender, :inclusion => ["m", "f", "M", "F"]
