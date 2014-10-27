8e5b38a22cc8a548f1bdd709b85247faff829878
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-27 16:05:07 -0400

    added methods in round that are called in player show view. updated player show view to include round score, par; updated link to start a new round

--- app/models/round.rb
+++ app/models/round.rb
@@ -8,4 +8,26 @@ class Round < ActiveRecord::Base
 	validates :round_length, presence: true, numericality: {only_integer: true}
 	validates :player_id, presence: true, numericality: {only_integer: true}
 	validates :start_hole, presence: true, inclusion: { in: 1..18 }
+
+	def total
+		round_total = 0
+		scores.each do |score|
+			round_total += score
+		end
+		return round_total
+	end
+
+	def par
+		par = 0
+		scores.each do |score|
+			par += score.hole.par
+		end
+	end
+
+	def handicap_score
+		 handicap_total = 0
+		 scores.each do |score|
+		 	#do calculations here
+		 end
+	end
 end
