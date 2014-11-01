03c6db295e1213fb1f8101e55f800a4e54699a2d
Kerry Mui <kerry.t.mui@gmail.com>
2014-10-31 21:15:49 -0400

    added username uniqueness validator to player model, created passing tests for username validity and username uniqueness

--- app/models/player.rb
+++ app/models/player.rb
@@ -6,6 +6,8 @@ class Player < ActiveRecord::Base
 	before_create :create_remember_token
 
 	validates :username, presence: true, length: { maximum: 20 }
+	validates :username, uniqueness: true
+	
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 30 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,49 @@
 require 'spec_helper'
 
 describe Player do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a username" do
+  	player = Player.new(
+  		username: "Jack Johnson",
+  		email: "jackjohnson@gmail.com",
+  		password: "testtest",
+  		password_confirmation: "testtest",
+  		handicap_index: 8.0,
+  		gender: "m"
+  		)
+  	expect(player).to be_valid
+  end
+
+  it "is invalid with a duplicate name" do
+  	Player.create(
+  		username: "Jack Johnson",
+  		email: "jackjohnson@gmail.com",
+  		password: "testtest",
+  		password_confirmation: "testtest",
+  		handicap_index: 7.0,
+  		gender: "m"
+  		)
+  	player = Player.create(
+  		username: "Jack Johnson",
+  		email: "jack@gmail.com",
+  		password: "testing",
+  		password_confirmation: "testing",
+  		handicap_index: 8.5,
+  		gender: "f"
+  		)
+  	expect(player).to have(1).errors_on(:username)
+  end
+
+  it "is valid with a username under 20 characters"
+  it "is valid with an email"
+  it "is valid with an email"
+  it "is valid with an email under 30 characters"
+  it "is invalid with an email that's case-sensitive "
+  it "is invalid with an email non-standard format"
+  it "is valid with a gender of 'm'"
+  it "is valid with a gender of 'M'"
+  it "is valid with a gender of 'f'"
+  it "is valid with a gender of 'F'"
+  it "is valid with a password length >= 6 characters"
+  it "is valid with a handicap_index of a float"
+  
 end
