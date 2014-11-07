e7ba4bf64014ce35d36b927e8903df77a32bafca
Kerry Mui <kerry.t.mui@gmail.com>
2014-11-07 14:03:23 -0500

    aded create action w/ bug for saving; currently rendering new

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -6,7 +6,24 @@ class ScoresController < ApplicationController
 
 	end
 
+	def create
+	 @player = Player.find(params[:player_id])
+     @round = Round.find(params[:round_id])
+     @score = @round.scores.build(params[:score_params])
+      if @score.save
+      	redirect_to player_round_url(@player,@round)
+      else
+        render :new
+      end
+	end
+	
 	def edit
 		@score = Score.find(params[:id])
+	end
+
+	private
+
+	def score_params
+		params.require(:score).permit(:hole_id, :round_id, :score)
 	end
 end
