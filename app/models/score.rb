646277fe06ddfd8f059f8de45cd0daa0a17b5357
Kerry Mui <kerry.t.mui@gmail.com>
2014-11-07 10:08:50 -0500

    updated score model with method to get yards, and round show view to display yards

--- app/models/score.rb
+++ app/models/score.rb
@@ -4,4 +4,11 @@ class Score < ActiveRecord::Base
 
 	validates :hole_id, presence: true, numericality: {only_integer: true}
 	validates :round_id, presence: true, numericality: {only_integer: true}
+
+	def get_yards 
+		#Want current score to find it's hole_id thank look for the yards
+		self.hole.yardages.each do |yardage|
+			Yardage.where(score_id: self.id, hole_id: hole.id, yards: yardage)
+		end
+	end
 end
