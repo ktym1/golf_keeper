3c642cd716f674525a061cd8f3c17b15987999e9
Kerry Mui <kerry.t.mui@gmail.com>
2014-11-11 18:47:03 -0500

    created time_update method that's incomplete for returning a single record, currently returning relationships; added score_differential method for score total and par in round model and view

--- app/models/round.rb
+++ app/models/round.rb
@@ -13,6 +13,7 @@ class Round < ActiveRecord::Base
 	validates :player_id, presence: true, numericality: {only_integer: true}
 	validates :start_hole, presence: true, inclusion: { in: 1..18 }
 
+	#need to capture nil score field since nil can't be added to
 	def total
 		round_total = 0
 		scores.each do |score_model|
@@ -36,10 +37,30 @@ class Round < ActiveRecord::Base
 		 end
 	end
 
-	  def create_scores
-	  	# take this round's course, and create a nil score for each hole
-	  	self.course.holes.each do |hole|
-	  		Score.find_or_create_by(round_id: self.id, hole_id: hole.id)
-	  	end
-	  end
+	def create_scores
+		# take this round's course, and create a nil score for each hole
+		self.course.holes.each do |hole|
+			Score.find_or_create_by(round_id: self.id, hole_id: hole.id)
+		end
+	end
+
+	def time_update
+	#check this Round's (self) score updated_at attribute; puts this attribute if more recent than round's updated_at
+		self.scores.each do |score|
+			if score.updated_at > self.updated_at
+				puts score.updated_at
+			end
+		end
+	end
+
+	def score_differential
+		#Take current player round's total score and compare it to par.
+		if total > par
+			total - par
+		elsif total < par
+			par - total
+		else
+			"N/A"
+		end
+	end
 end
