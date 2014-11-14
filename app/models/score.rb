a2863b9b1c00f86615c0e259de5047978a406db1
Kerry Mui <kerry.t.mui@gmail.com>
2014-11-14 09:25:51 -0500

    added edit form, refactored new score form w/ method calls, updated score model w/ delegate method

--- app/models/score.rb
+++ app/models/score.rb
@@ -5,9 +5,9 @@ class Score < ActiveRecord::Base
 	validates :hole_id, presence: true, numericality: {only_integer: true}
 	validates :round_id, presence: true, numericality: {only_integer: true}
 
+	delegate :player, to: :round, allow_nil: true
+	
 	def get_yards 
-		#Want current score to find it's hole_id than look for the yards
-		#For any score, the yards will correspond to the tee they're playing off the round
 		yardages = self.hole.yardages.where("tee_id = ?", self.round.tee_id).first
 		yardages.yards
 	end
