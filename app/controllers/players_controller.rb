a3710b2137e915e9685eba49bfae49552b05377b
Kerry Mui <kerry.t.mui@gmail.com>
2014-11-14 16:37:28 -0500

    updated player controller with before_action filter to find player_id, udpated player edit form and partial by removing password confirmation

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,16 @@
 class PlayersController < ApplicationController
  before_action :signed_in_player, only: [:edit, :update]
  before_action :correct_player, only: [:edit, :update]
+ before_action :get_player, :only => [:show, :edit, :update]
+  
+  def get_player
+    begin
+      @player = Player.find(params[:id])
+    rescue
+      render 'shared/not_found'
+    end
+  end
+
   def new
   	@player = Player.new
   end
@@ -16,16 +26,8 @@ class PlayersController < ApplicationController
   	end
   end  
 
-  def show
-    @player = Player.find(params[:id])
-  end
-  
-  def edit
-  end
-  
   def update
     remove_password
-    @player = Player.find(params[:id])
     if @player.update_attributes(player_params)
       flash[:success] = "Profile updated"
       redirect_to @player
@@ -34,21 +36,24 @@ class PlayersController < ApplicationController
     end
   end
 
-  private
-  #Please, change this method name. I ain't creative now
-  def remove_password
-    if player_params[:password].blank?
-       player_params.delete("password")
-       player_params.delete("password_confirmation")
-    end
+  def show
   end
-
-  def destroy
-    #how to consider admin rights for deletion
+  
+  def edit
   end
- 
+  
     private
+      def remove_password
+        if player_params[:password].blank?
+           player_params.delete("password")
+           player_params.delete("password_confirmation")
+        end
+      end
 
+      def destroy
+        #how to consider admin rights for deletion
+      end
+   
       def player_params
       	params.require(:player).permit(:email, :username, :password, :password_confirmation, :handicap_index, :gender)
       end
