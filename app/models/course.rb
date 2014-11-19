cf11e4a8c61d3e6ae2e3094d09bade75c341ee1d
Kerry Mui <kerry.t.mui@gmail.com>
2014-11-18 23:02:32 -0500

    removed availability method in course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,9 +5,4 @@ class Course < ActiveRecord::Base
 	has_many :players, through: :rounds
 
 	validates :name, presence: true, uniqueness: true
-
- 	def availability(number_of_golfers, reservation_time)
- 		reserve_size = reservations.where(reservation_time: reservation_time).sum(:number_of_golfers)
- 		reserve_size + number_of_golfers <= capacity
- 	end	
 end
