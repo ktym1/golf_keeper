ca360c262a94fc43065cb6e52ba849baf9a2fee2
Kerry Mui <kerry.t.mui@gmail.com>
2014-11-20 12:43:55 -0500

    dropped db and migrated w/o seed file. updated new player form w/ password and password confirmation fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119020250) do
+ActiveRecord::Schema.define(version: 20141104041941) do
 
   create_table "courses", force: true do |t|
     t.string   "name"
