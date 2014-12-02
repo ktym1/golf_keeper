6a8a640ad3acab92263084524e0ce8ca6a798309
Kerry Mui <kerry.t.mui@gmail.com>
2014-12-02 12:22:02 -0500

    updated hole model validations w/ blank field handling

--- app/models/hole.rb
+++ app/models/hole.rb
@@ -4,16 +4,16 @@ class Hole < ActiveRecord::Base
 	has_many :yardages, dependent: :destroy
 
 	validates :course_id, presence: true, numericality: {only_integer: true}
-	validates :par, presence: true, numericality: {only_integer: true}
+	validates :par, presence: true, numericality: {only_integer: true}, :if => Proc.new {|h| not h.par.blank?}
 
 	validates :hole_number, inclusion: { in: 1..18, message: "Must be from 1 to 18" } 
 	validates :hole_number, uniqueness: { scope: :gender,
     message: "should be unique for this course's gender" }
     
-    validates :handicap_rating, presence: true, numericality: {only_integer: true}
-    validates :handicap_rating, inclusion: { in: 1..18, message: "Must be from 1 to 18" } 
+    validates :handicap_rating, numericality: {only_integer: true}, :if => Proc.new {|h| not h.handicap_rating.blank?}
+    validates :handicap_rating, inclusion: { in: 1..18, message: "Must be from 1 to 18" }, :if => Proc.new {|h| not h.handicap_rating.blank?}
     validates :handicap_rating, uniqueness: { scope: :gender,
-    message: "should be unique for this course's gender" }
+    message: "should be unique for this course's gender" }, :if => Proc.new {|h| not h.handicap_rating.blank?}
     
     validates :gender, :inclusion => ["m", "f", "M", "F"]
     accepts_nested_attributes_for :yardages, :reject_if => :all_blank, :allow_destroy => true
