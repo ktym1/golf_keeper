bde3446b40dd6a1e93c9eb9845154bf93874224b
Kerry Mui <kerry.t.mui@gmail.com>
2014-12-09 18:07:45 -0500

    added corresponding associations between match and round

--- app/models/round.rb
+++ app/models/round.rb
@@ -7,7 +7,8 @@ class Round < ActiveRecord::Base
 	belongs_to :tee
 	has_many :scores, dependent: :destroy
 	has_many :holes, through: :scores
-	
+	has_many :player_matches
+		
 	validates :course_id, presence: true, numericality: {only_integer: true}
 	validates :round_length, presence: true, numericality: {only_integer: true}
 	validates :player_id, presence: true, numericality: {only_integer: true}
