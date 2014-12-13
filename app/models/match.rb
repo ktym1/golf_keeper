6b3c0c07e95baa4f7155fac9794858f972ccd0d1
Kerry Mui <kerry.t.mui@gmail.com>
2014-12-12 20:41:07 -0500

    updated match model with associations

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,3 +1,19 @@
 class Match < ActiveRecord::Base
+	after_save: :add_player_to_playermatch
 	
+	belongs_to :creating_player, class_name: "Player", foreign_key: :player_id
+	has_many :playermatches
+	has_many :joined_player, through: :playermatches, source: :player
+
+	validate :maximum_players
+
+	def add_player_to_playermatch
+		self.playermatches.create(player_id: self.player.id, match_id: self.id)
+	end
+
+	def maximum_players
+		if joined_player > 4
+			errors.base("Maximum 4 players")
+		end
+	end
 end
