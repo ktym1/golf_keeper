910d59c921328ee07b3b76534cb2e95ae3e190d5
Kerry Mui <kerry.t.mui@gmail.com>
2014-12-15 19:59:54 -0500

    fixed bug by updating add_player_to_player_match method w/ correst player_id field

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,19 +1,19 @@
-class Match < ActiveRecord::Base
-	after_save :add_player_to_player_match
-	
+class Match < ActiveRecord::Base	
 	belongs_to :creating_player, class_name: "Player", foreign_key: :player_id
 	has_many :player_matches
-	has_many :joined_player, through: :player_matches, source: :player
+	has_many :joined_players, through: :player_matches, source: :player
 
-	validate :maximum_players
+	after_save :add_player_to_player_match
+
+	# validate :maximum_players
 
 	def add_player_to_player_match
-		self.player_matches.create(player_id: self.player.id, match_id: self.id)
+		self.player_matches.create(player_id: self.player_id, match_id: self.id)
 	end
 
 	def maximum_players
-		if joined_player > 4
-			errors.base("Maximum 4 players")
+		if joined_players > 4
+			errors.add(:joined_players, "Maximum 4 players")
 		end
 	end
 end
