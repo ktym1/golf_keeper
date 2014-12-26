13cef30c8638f03e86231ab50a66e2b2339193af
Kerry Mui <kerry.t.mui@gmail.com>
2014-12-26 16:51:34 -0500

    updated player_match routes, created match show view, and updated player_match w/ create_round function

--- app/models/player_match.rb
+++ app/models/player_match.rb
@@ -2,5 +2,8 @@ class PlayerMatch < ActiveRecord::Base
 	belongs_to :round
 	belongs_to :match
 	belongs_to :player
-end
 
+	def create_round
+		#if round for this match & player exists, use it, otherwise create a new empty round for this player on the challenged course and tee
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   
   resources :players do
     resources :matches 
+      resource :player_matches, only: [:create, :update]
   end
 
   resources :players do
