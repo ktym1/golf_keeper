b4b5acfd18e4131a310562d1077c08ac7b657bd8
Kerry Mui <kerry.t.mui@gmail.com>
2015-01-07 17:22:59 -0500

    updated custom validation for max players; moved validation from match to challenge model

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -4,8 +4,18 @@ class Challenge < ActiveRecord::Base
 	belongs_to :player
 	after_save :create_round
 
+	validate :maximum_players_per_match
+
+	def maximum_players_per_match
+		if self.match.joined_players.count > 3
+			errors[:base] << "Maximum 4 players"
+		end
+	end
+
 	def create_round
 		round = Round.create(course_id: self.match.course_id, player_id: self.player.id, tee_id: self.match.tee_id, start_hole: 1, round_length: 18)
 		self.update_column(:round_id, round.id)
 	end
+
+
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -4,12 +4,5 @@ class Match < ActiveRecord::Base
 	belongs_to :creating_player, class_name: "Player", foreign_key: :player_id
 	has_many :challenges
 	has_many :joined_players, through: :challenges, source: :player
-
-	# validate :maximum_players	
-
-	def maximum_players
-		if joined_players > 4
-			errors.add(:joined_players, "Maximum 4 players")
-		end
-	end
+	
 end
