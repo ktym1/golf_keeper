c175a7e97d976e72ea868137bde40e19da47feae
Kerry Mui <kerry.t.mui@gmail.com>
2015-01-09 13:03:37 -0500

    incomplete challenge scorecard. added scope in match model

--- app/models/match.rb
+++ app/models/match.rb
@@ -4,5 +4,11 @@ class Match < ActiveRecord::Base
 	belongs_to :creating_player, class_name: "Player", foreign_key: :player_id
 	has_many :challenges
 	has_many :joined_players, through: :challenges, source: :player
-	
+  has_many :rounds, through: :challenges	
+
+  def player_challenge 
+    c = self.challenges.where("player_id = ?", 1).first
+    c.rounds.scores
+  end
+
 end
