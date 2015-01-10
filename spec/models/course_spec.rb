c2702517cb5f557dfbe2e26fcf6424e2b57b1b5f
Kerry Mui <kerry.t.mui@gmail.com>
2015-01-10 11:24:22 -0500

    updated .rspec file to format documentation; updated course_spec w/ passing tests for validation of name and course length attributes

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,19 +1,19 @@
 require 'spec_helper'
 
 describe Course do
-  let(:course) { Course.new(name: "Glen Abbey", capacity: 18)}
   
-  it "is valid with a name" do
+  it "is valid with a name and course_length" do
   	course = Course.new(
-  		name: "Glen Abbey")
+  		name: "Glen Abbey",
+      course_length: 18)
   	expect(course).to be_valid
   end
 
   it "is invalid with a duplicate name" do
   	Course.create(
-  		name: "Glen Abbey")
-  	course = Course.create(
-  		name: "Glen Abbey")
+  		name: "Glen Abbey", course_length: 18)
+  	course = Course.new(
+  		name: "Glen Abbey", course_length: 18)
   	expect(course).to have(1).errors_on(:name)
   end
 end
