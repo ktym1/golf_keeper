1177e67b49613948186f75325bc0d1872d02e945
Kerry Mui <kerry.t.mui@gmail.com>
2015-01-09 20:00:44 -0500

    removed association in tee for ratings; generated migration to remove ratings table

--- app/models/tee.rb
+++ app/models/tee.rb
@@ -1,6 +1,5 @@
 class Tee < ActiveRecord::Base
 	has_many :yardages
-	has_many :ratings
 	has_many :rounds
 	belongs_to :course
 	has_many :matches
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141229180539) do
+ActiveRecord::Schema.define(version: 20150110003316) do
 
   create_table "challenges", force: true do |t|
     t.integer  "round_id"
