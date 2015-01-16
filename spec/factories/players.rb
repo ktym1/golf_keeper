d4c2113f35f090c7817d1b687c5849805de26c8c
Kerry Mui <kerry.t.mui@gmail.com>
2015-01-15 20:19:07 -0500

    removed course association from player factory; updated player_spec w/ test for valid factory

--- spec/factories/players.rb
+++ spec/factories/players.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
   factory :player do
-    association :player
-      username "kmui"
-      email "kerrymui@test.com"
-      password "testing"
-      password_confirmation "testing"
-      handicap_index 10
-      gender "M"
+    username "kmui"
+    email "kerrymui@test.com"
+    password "testing"
+    password_confirmation "testing"
+    handicap_index 10
+    gender "M"
+
   end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,53 +1,58 @@
 require 'spec_helper'
 
 describe Player do
-  it "is valid with a username" do
-  	player = Player.new(
-  		username: "Jack Johnson",
-  		email: "jackjohnson@gmail.com",
-  		password: "testtest",
-  		password_confirmation: "testtest",
-  		handicap_index: 8.0,
-  		gender: "m"
-  		)
-  	expect(player).to be_valid
-  end
-
-  it "is invalid without a username" do
-  	expect(Player.new(username: nil)).to have(1).errors_on(:username)
-  end
   
-  it "is invalid with a duplicate name" do
-  	Player.create(
-  		username: "Jack Johnson",
-  		email: "jackjohnson@gmail.com",
-  		password: "testtest",
-  		password_confirmation: "testtest",
-  		handicap_index: 7.0,
-  		gender: "m"
-  		)
-  	player = Player.create(
-  		username: "Jack Johnson",
-  		email: "jack@gmail.com",
-  		password: "testing",
-  		password_confirmation: "testing",
-  		handicap_index: 8.5,
-  		gender: "f"
-  		)
-  	expect(player).to have(1).errors_on(:username)
+  it "has a valid factory" do 
+    expect(build(:player)).to be_valid
   end
 
-  it "is valid with a username under 20 characters" do
-  	player = Player.new(
-  		username: "123456789123456789123456789",
-  		email: "jack@gmail.com",
-  		password: "testing",
-  		password_confirmation: "testing",
-  		handicap_index: 8.5,
-  		gender: "f"
-  		)
-  	expect(player).to have(1).errors_on(:username)
-  end
+  # it "is valid with a username" do
+  # 	player = Player.new(
+  # 		username: "Jack Johnson",
+  # 		email: "jackjohnson@gmail.com",
+  # 		password: "testtest",
+  # 		password_confirmation: "testtest",
+  # 		handicap_index: 8.0,
+  # 		gender: "m"
+  # 		)
+  # 	expect(player).to be_valid
+  # end
+
+  # it "is invalid without a username" do
+  # 	expect(Player.new(username: nil)).to have(1).errors_on(:username)
+  # end
+  
+  # it "is invalid with a duplicate name" do
+  # 	Player.create(
+  # 		username: "Jack Johnson",
+  # 		email: "jackjohnson@gmail.com",
+  # 		password: "testtest",
+  # 		password_confirmation: "testtest",
+  # 		handicap_index: 7.0,
+  # 		gender: "m"
+  # 		)
+  # 	player = Player.create(
+  # 		username: "Jack Johnson",
+  # 		email: "jack@gmail.com",
+  # 		password: "testing",
+  # 		password_confirmation: "testing",
+  # 		handicap_index: 8.5,
+  # 		gender: "f"
+  # 		)
+  # 	expect(player).to have(1).errors_on(:username)
+  # end
+
+  # it "is valid with a username under 20 characters" do
+  # 	player = Player.new(
+  # 		username: "123456789123456789123456789",
+  # 		email: "jack@gmail.com",
+  # 		password: "testing",
+  # 		password_confirmation: "testing",
+  # 		handicap_index: 8.5,
+  # 		gender: "f"
+  # 		)
+  # 	expect(player).to have(1).errors_on(:username)
+  # end
 
 #   it "is valid with an email" do
 #   		player = Player.new(
